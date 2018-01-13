20549b333ee07d574715538c43c0b194f61cc58f
rob <robert.aleman@pomona.edu>
2018-01-13 00:12:31 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# discord-cryptocurrency-bot
-Cryptocurrency info gathering bot for Discord chat platform; checks prices, exchange data, set alerts, and more
+# CryptoBot for Discord
+A bot for [Discord](https://discordapp.com/) that provides utilities for cryptocurrency traders. Run it in your Discord server and users can call various commands that fetch current information from crypto-exchanges like Binance. CryptoBot uses Node.js and the [Node Binance API wrapper](https://github.com/jaggedsoft/node-binance-api). 
+
+## Bot Setup
+Install the latest versions of Node.js and the [Node Binance API wrapper](https://github.com/jaggedsoft/node-binance-api). Clone or download this repository. Replace the token in the `auth.json` with the your own token genereated from the [app section of your Discord account](https://discordapp.com/developers/applications/me). Add the bot to your server.
+
+When you're ready, navigate to this repo and run:
+```
+node bot.js
+```
+
+Your bot should be ready to go.
+
+
+## Commands
+
+`!help` provides a list of commands available to the user
+`!vol <MARKET>` returns the current 24-hour volume of a specified market on Binance 
+
--- bot.js
+++ bot.js
@@ -1,3 +1,76 @@
-// set-up; defines dependencies
+// -----------------------
+// SET-UP AND DEPENDENCIES
+// -----------------------
+
 var Discord = require('discord.io');
 var auth = require('./auth.json');
+
+// Loads market list.
+const markets = require('./markets.json');
+
+// Access Node-Binance API package and configures Binance API
+const binance = require('node-binance-api');
+binance.options({
+  'APIKEY':'<key>',
+  'APISECRET':'<secret>'
+});
+
+
+// -----------------------
+// DISCORD BOT
+// -----------------------
+
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+
+
+// actions that launch upon bot startup
+bot.on('ready', function (evt) {
+    console.log('Connected');
+    console.log('Logged in as: ' + bot.username + '-' + bot.id);
+});
+
+
+// actions that launch upon any messages in the channel
+bot.on('message', function (user, userID, channelID, message, evt) {
+
+    // interprets any message starting with a '!' as a command
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var command = args[0];
+        var param = args[1]
+
+        // help command
+        if (command == "help") {
+          bot.sendMessage({
+            to: channelID,
+            message: "This is CryptoBot! Here's a list of commands. \n" +
+            "!vol MARKET : gives the volume for a specified market"
+          });
+        }
+
+        // volume command
+        if (command == "vol") {
+          var market = param
+          binance.prevDay(market, function(prevDay, symbol) {
+              bot.sendMessage({
+                to: channelID,
+                message: "24h volume: " + prevDay.volume.toString()
+              });
+          });
+         }
+     }
+});
+
+
+// -----------------------
+// CONFIGURATION FUNCTIONS
+// -----------------------
+
+function refreshMarkets() {
+    binance.prices(function(ticker) {
+        console.log(JSON.stringify(Object.keys(ticker)).length);
+    });
+}
